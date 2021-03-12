9ac43f540dc787a30ed3726b5115cd03461869ae
lisa2 <lisadsa24@gmail.com>
2021-03-12 16:23:09 +0530

    basic menu

---
+++ menu_display_atom.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int choice;
+	puts("/=============================================/");
+  ōōputs("                    MENU                       ");
+	puts("/=============================================/");
+	puts("1. Reserve seats");
+	puts("2. Search your ticket");
+	puts("3. Check bus fare");
+	puts("4. Contact us");
+	puts("5. Exit");
+	printf(">> ");
+	scanf("%d", &choice);
+  return 0;
+}
